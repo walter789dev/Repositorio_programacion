7cfa5073d2fc5bd34121a8e35de77dc6896ba49a
Walter Plaza <131600264+walter789dev@users.noreply.github.com>
2023-09-11 19:37:53 -0300

    Update Tp-for-while.py

--- Tp-for-while.py
+++ Tp-for-while.py
@@ -10,14 +10,17 @@ for i in range(5):
 
   for j in mensaje_lower:
     letra = letras.find(j)
-    search = letra + encriptacion
 
-    if search >= 27:
-      resto = search % 27
-      mensaje_encrip += letras[resto]
+    if letra <= -1: 
+       mensaje_encrip += j
     else: 
-      mensaje_encrip += letras[letra + encriptacion]
-      
+      search = letra + encriptacion
+      if search >= 27:
+         resto = search % 27
+         mensaje_encrip += letras[resto]
+      else: 
+         mensaje_encrip += letras[letra + encriptacion]
+
   print(f"Tu mensaje es: {mensaje_encrip.upper()}")
   mensaje_encrip = ""
 
@@ -32,16 +35,21 @@ while val_1 != 0:
   if number == 0:
     val_1 = 0
   else:
-     t_number = list(str(number))
-     i = 0
-     while i < (len(t_number)):
-        if (int(t_number[i]) % 2) == 0:
-           d_par += 1
-           i += 1
-        else:
-           d_impar += 1
-           i += 1
-
+      t_number = list(str(number))
+      i = 0
+      d2_par = 0
+      d2_impar = 0
+      while i < (len(t_number)):
+         if (int(t_number[i]) % 2) == 0:
+            d2_par += 1
+            d_par += 1
+            i += 1
+         else:
+            d2_impar += 1
+            d_impar += 1
+            i += 1
+      print(f"El numero posee {d2_par} digitos par y {d2_impar} digitos impar")
+      
 print(f"Digitos Pares: {d_par}")
 print(f"Digitos Impares: {d_impar}")
         
